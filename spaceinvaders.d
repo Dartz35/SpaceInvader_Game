d531da47623d8a886d4ef39a8e17a5a0f3be6736
Ali Khalifa <aliHAkhalifa09@yahoo.com>
2024-07-12 16:15:20 -0400

    first commit

---
+++ spaceinvaders.d
@@ -0,0 +1,6 @@
+.\spaceinvaders.o: SpaceInvaders.c
+.\spaceinvaders.o: Nokia5110.h
+.\spaceinvaders.o: ..//tm4c123gh6pm.h
+.\spaceinvaders.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\spaceinvaders.o: Random.h
+.\spaceinvaders.o: TExaS.h
---
+++ wavfile.d
@@ -0,0 +1,1 @@
+.\wavfile.o: WavFile.c
